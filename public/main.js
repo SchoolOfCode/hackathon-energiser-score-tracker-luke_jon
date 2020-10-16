a34d503209d73b0434f667bc01b3180102157ba8
Jon Owens <jonowens84@gmail.com>
2020-10-16 11:55:58 +0100

    css with hover for table and table to contain all

--- public/main.js
+++ public/main.js
@@ -2,6 +2,11 @@
 // use query selector to select the section in js
 const tableSection = document.querySelector("#table");
 const totalTable = document.querySelector("#total");
+const refrshTable = document.querySelector("#updateTable");
+const submitButton = document.querySelector("button[type='submit']");
+
+
+
 
 // write an async function to fetch data from our database
 
@@ -25,16 +30,45 @@ function createTable(user) {
 // deconstruct the payload array
 // reset the innerhtml "" of table section
 
-function createUserRows({ name, total }) {
+function createUserRows({
+  name,
+  mind,
+  gartic,
+  scavenger_hunt,
+  scattergories,
+  movie_game,
+  total,
+}) {
   const row = document.createElement("tr");
   const tdUserName = document.createElement("td");
+
+  const tdMind = document.createElement("td");
+  const tdGartic = document.createElement("td");
+  const tdScavengerHunt = document.createElement("td");
+  const tdScattergories = document.createElement("td");
+  const tdMovieGame = document.createElement("td");
   const tdTotal = document.createElement("td");
   tdUserName.innerText = name;
+
+  tdMind.innerText = mind;
+  tdGartic.innerText = gartic;
+  tdScavengerHunt.innerText = scavenger_hunt;
+  tdScattergories.innerText = scattergories;
+  tdMovieGame.innerText = movie_game;
   tdTotal.innerText = total;
+
   row.appendChild(tdUserName);
+  row.appendChild(tdMind);
+  row.appendChild(tdMind);
+  row.appendChild(tdGartic);
+  row.appendChild(tdScavengerHunt);
+  row.appendChild(tdScattergories);
+  row.appendChild(tdMovieGame);
   row.appendChild(tdTotal);
   return row;
 }
+
+const gameSelectorButton = 
 
 getTable();
 // write another function to append each item on the array to the dom
